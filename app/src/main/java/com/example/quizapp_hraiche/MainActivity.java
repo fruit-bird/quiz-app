6af905dbaffbc7ee426edf8095a93860b046f51b
fruit-bird <github.ru3w9@slmail.me>
2024-03-13 14:15:10 +0000

    controller stuff

--- app/src/main/java/com/example/quizapp_hraiche/MainActivity.java
+++ app/src/main/java/com/example/quizapp_hraiche/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.quizapp_hraiche;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+//    1. declaration
+    EditText etEmail, etPassword;
+    Button bLogin;
+    TextView tvRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // associe la layout a un controlleur
         setContentView(R.layout.activity_main);
+
+//        2. recuperation des ids
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        bLogin = findViewById(R.id.bLogin);
+        tvRegister = findViewById(R.id.tvRegister);
+
+//        3. association des listeners
+        bLogin.setOnClickListener(v -> {
+//            4. traitement
+            if (etEmail.getText().toString().equals("admin") && etPassword.getText().toString().equals("admin")) {
+                startActivity(new Intent(MainActivity.this, Quiz1.class));
+            } else {
+                Toast.makeText(MainActivity.this, "Invalid email or password", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tvRegister.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, Register.class));
+        });
     }
 }
--- app/src/main/java/com/example/quizapp_hraiche/Register.java
+++ app/src/main/java/com/example/quizapp_hraiche/Register.java
@@ -2,13 +2,34 @@ package com.example.quizapp_hraiche;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
+    EditText etUsername, etEmail, etPassword, etConfirmPassword;
+    Button bRegister;
+    TextView tvRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etUsername = findViewById(R.id.etUsername);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        etConfirmPassword = findViewById(R.id.etConfirmPassword);
+        bRegister = findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(v -> {
+            if (etPassword.getText().toString().equals(etConfirmPassword.getText().toString())) {
+                startActivity(new Intent(Register.this, Quiz1.class));
+            }
+        });
+
+        tvRegister.setOnClickListener(v -> startActivity(new Intent(Register.this, MainActivity.class)));
     }
 }
